53699943d4992355f33147170800d63c8bb631e9
bmmaker <bmmaker35@gmail.com>
2021-06-16 21:02:52 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
 
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
 
 def single_post_page(request, pk):
@@ -21,6 +15,6 @@ def single_post_page(request, pk):
         request,
         'blog/single_post_page.html',
         {
-            'post':post,
+            'post': post,
         }
     )
