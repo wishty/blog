e962171553040cf518ed505446912b2508bac13a
bmmaker <bmmaker35@gmail.com>
2021-06-28 16:58:44 +0900

    로그 파일 적용하기3

--- board/views.py
+++ board/views.py
@@ -5,9 +5,10 @@ from django.http import HttpResponse
 logger = logging.getLogger('board')
 
 def index(request):
+    logger.info("INFO 레벨로 출력")
     return HttpResponse("안녕하세요, 게시판 페이지에 오신 것을 환영합니다. ^^")
 
-    logger.info("INFO 레벨로 출력")
+
 
 
 # def index(request):
