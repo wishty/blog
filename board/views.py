1b7ea29a14865eb59ea59d8ffb47004f6591d483
bmmaker <bmmaker35@gmail.com>
2021-06-28 16:28:40 +0900

    로그 적용

--- board/views.py
+++ board/views.py
@@ -1,9 +1,14 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 
+logger = logging.getLogger('board')
 
 def index(request):
     return HttpResponse("안녕하세요, 게시판 페이지에 오신 것을 환영합니다. ^^")
+
+    logger.info("INFO 레벨로 출력")
+
 
 # def index(request):
 #     return render(
